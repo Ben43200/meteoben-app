2ddf925c05668a31ceef01c043c77634d220cc41
Ben <meteoben@gmail.com>
2023-11-09 13:13:43 +0100

    corrections tournures de phrase et fautes du texte de p

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -16,21 +16,21 @@ export default function Home() {
             <h2>Développeur Web en biovallée de la Drôme</h2>
             <div className="main-container">
                 <p>Bienvenue sur mon site</p>
-                <p>Je me présente, je m'appelle Benjamin, j'ai suivi une formation de développeur web pendant laquelle j'ai eu à réaliser différents projets et que j'ai terminé avec succés.
+                <p>Je me présente, je m'appelle Benjamin, j'ai suivi une formation de développeur web pendant laquelle j'ai eu à réaliser différents projets et que j'ai terminée avec succès.
                     J'utilise différents langages de programmation tel que le html, le css , et le javascript, j'ai égalmement des compétences en Node.js et en React.
-                    Je continue à me former tout les jours en me tenant au courant des nouveautés concernant les différents langages de programmation.</p>
+                    Je continue à me former tous les jours en me tenant au courant des nouveautés concernant les différents langages de programmation.</p>
                 <p>J'ai crée ma micro entreprise au début de ce mois de novembre 2023, j'ai déjà fait par ailleurs un projet pour une association.</p>
-                <p>Je vous propose donc de créer votre site clé en main, je me charge de coder votre site web, il sera référencé sur google grâce au SEO que je mettrai en place et je
+                <p>Je vous propose donc de créer votre site clé en main, je me charge de le coder, il sera référencé sur google grâce au SEO que je mettrai en place et je
                     ferai en sorte qu'il soit adapté en mobile, tablette et ordinateur.
                     Les gens non avertis ne le savent pas forcément, mais le référencement est beaucoup plus efficace avec une version mobile en parallèle, car la plupart des gens utilisent
                     leur mobile pour naviguer sur internet maintenant, et ça, google l'a bien compris.
-                    Je me charge de vous louer un espace de location sur mon serveur pro pour y faire tourner votre site.
-                    Je vous laisse de votre côté réserver un nom de domaine si vous en avez pas déjà un, vous restez ainsi propriétaire de votre nom de domaine, j'ai déjà vu pas le passé
+                    Je me charge de vous louer un espace sur mon serveur pro pour y faire tourner votre site.
+                    Je vous laisse de votre côté réserver un nom de domaine si vous n'en avez pas déjà un, vous restez ainsi propriétaire de votre nom de domaine.J'ai déjà vu par le passé
                     des développeurs acheter le nom de domaine pour des clients et ne pas les restituer si ils ne voulaient pas poursuivre leur partenariat par exemple.
                     Je peux vous aider dans vos démarches.
                 </p>
-                <p>Je suis développeur web alors votre site sera unique, il est fait à l'aide de lignes de code uniquement, je n'utilise pas d'outils tel que Wordpress par exemple.</p>
-                <p>Chaque projet est différent et ne sera pas appréhendé de la même manière,il y a des projets simples et d'autres qui demandent beaucoup plus de travail,
+                <p>Votre site sera unique, il sera fait à l'aide de lignes de code uniquement, je n'utilise pas d'outils tel que Wordpress par exemple.</p>
+                <p>Chaque projet est différent et sera adapté à vos souhaits, il y a des projets simples et d'autres qui demandent beaucoup plus de travail,
                     c'est pourquoi, je vous propose
                     dès à présent de prendre contact avec moi afin qu'on en discute.</p>
             </div>
