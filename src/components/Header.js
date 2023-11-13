833d8cf46c2a0651656ef550c5362e0cf710d066
Ben <meteoben@gmail.com>
2023-11-13 09:34:35 +0100

    ajout header tag to header js supression div entourantle header

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,22 +13,24 @@ import React from "react";
 
 
 export default function Header() {
-    return (<div>
-        <div className="title-container">
-            <h1>Meteoben</h1>
-            <h2>Développeur Web en biovallée de la Drôme</h2>
-            {/* <img src={background} alt="background" className="fond-home" /> */}
-            <img alt=''
-                src={require('../compress-image-large.webp')}
-                srcSet={`
+    return (
+        <header>
+            <div className="title-container">
+                <h1>Meteoben</h1>
+                <h2>Développeur Web en biovallée de la Drôme</h2>
+                {/* <img src={background} alt="background" className="fond-home" /> */}
+                <img alt=''
+                    src={require('../compress-image-large.webp')}
+                    srcSet={`
 ${require('../compress-background-small.webp')} 320w, 
 ${require('../compress-background-medium.webp')} 680w,
 ${require('../compress-image-large.webp')} 1024w
 
 `} loading="lazy" />
 
-        </div>
-    </div>
+            </div>
+        </header>
+
     )
 
 }
