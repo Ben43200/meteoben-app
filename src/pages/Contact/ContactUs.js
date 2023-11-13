6605f5e6e7c6b361580701688a6963619a45f46b
Ben <meteoben@gmail.com>
2023-11-13 06:37:11 +0100

    suppression commentaire

--- src/pages/Contact/ContactUs.js
+++ src/pages/Contact/ContactUs.js
@@ -1,48 +1,10 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import "./contactus.scss"
-// import background from "../../compress-background.webp";
 import Navbar from '../../components/NavBar';
 import BackToUp from "../../components/BackToUp/BackToUp";
 import Header from '../../components/Header';
 
-
-// export const ContactUs = () => {
-//     const form = useRef();
-//     const sendEmail = (e) => {
-//         e.preventDefault();
-
-//         emailjs.sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", form.current, "YOUR_PUBLIC_KEY").then(
-//             (result) => {
-//                 console.log(result.text);
-//             },
-//             (error) => {
-//                 console.log(error.text);
-//             }
-//         );
-//     };
-
-//     return (
-//         <form ref={form} onSubmit={sendEmail}>
-//             <label>Name</label>
-//             <input type="text" name="user_name" />
-//             <label>Email</label>
-//             <input type="email" name="user_email" />
-//             <label>Message</label>
-//             <textarea name="message" />
-//             <input type="submit" value="Send" />
-//         </form>
-//     );
-// };
-
-// // export default ContactUs;
-
-
-
-
-
-// import React from 'react'
-// import './App.css';
 
 export const ContactUs = () => {
     const form = useRef();
@@ -87,8 +49,7 @@ export const ContactUs = () => {
             <BackToUp />
         </div>
     );
-    // }
-    // );
+
 }
 
 export default ContactUs;
